b461bc8b09a54019359bc7899da2e4123d13052f
Ashisoma <okalionesmus@gmail.com>
2020-11-27 11:52:33 +0300

    log message for success and failed

--- app/src/main/java/com/moringaschool/myrestaurants/ui/CreateAccountActivity.java
+++ app/src/main/java/com/moringaschool/myrestaurants/ui/CreateAccountActivity.java
@@ -16,6 +16,7 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.moringaschool.myrestaurants.R;
 
 import butterknife.BindView;
@@ -32,12 +33,14 @@ public class CreateAccountActivity extends AppCompatActivity implements View.OnC
     @BindView(R.id.loginTextView) TextView mLoginTextView;
 
     private FirebaseAuth mFirebaseAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
         ButterKnife.bind(this);
+        createAuthStateListener();
 
         mLoginTextView.setOnClickListener(this);
         mCreateUserButton.setOnClickListener(this);
@@ -75,4 +78,30 @@ public class CreateAccountActivity extends AppCompatActivity implements View.OnC
                 });
     }
 
+    private void createAuthStateListener(){
+        mAuthStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                final FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null){
+                    Intent intent = new Intent(CreateAccountActivity.this, MainActivity.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        };
+    }
+
+    @Override
+    public void onStart(){
+        super.onStart();
+        mFirebaseAuth.addAuthStateListener(mAuthStateListener);
+    }
+
+    @Override
+    public void onStop(){
+        super.onStop();
+        mFirebaseAuth.removeAuthStateListener(mAuthStateListener);
+    }
 }
