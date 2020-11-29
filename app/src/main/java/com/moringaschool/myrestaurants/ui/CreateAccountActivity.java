8af39f02a973f62ef94fa8465c99953b751ba18b
langat justus <justuslangat78@gmail.com>
2020-11-29 08:07:03 +0300

    validate log in

--- app/src/main/java/com/moringaschool/myrestaurants/ui/CreateAccountActivity.java
+++ app/src/main/java/com/moringaschool/myrestaurants/ui/CreateAccountActivity.java
@@ -3,6 +3,8 @@ package com.moringaschool.myrestaurants.ui;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -17,6 +19,7 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
 import com.moringaschool.myrestaurants.R;
 
 import butterknife.BindView;
@@ -31,9 +34,12 @@ public class CreateAccountActivity extends AppCompatActivity implements View.OnC
     @BindView(R.id.passwordEditText) EditText mPasswordEditText;
     @BindView(R.id.confirmPasswordEditText) EditText mConfirmPasswordEditText;
     @BindView(R.id.loginTextView) TextView mLoginTextView;
-
+    @BindView(R.id.passwordLoginButton) Button mPasswordLoginButton;
     private FirebaseAuth mFirebaseAuth;
     private FirebaseAuth.AuthStateListener mAuthStateListener;
+
+    private ProgressDialog mAuthProgressDialog ;
+    private String mName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,9 +47,12 @@ public class CreateAccountActivity extends AppCompatActivity implements View.OnC
         setContentView(R.layout.activity_create_account);
         ButterKnife.bind(this);
         createAuthStateListener();
+        createAuthProgressDialog();
 
         mLoginTextView.setOnClickListener(this);
         mCreateUserButton.setOnClickListener(this);
+        mPasswordLoginButton.setOnClickListener(this);
+
 
         mFirebaseAuth = FirebaseAuth.getInstance();
     }
@@ -57,6 +66,9 @@ public class CreateAccountActivity extends AppCompatActivity implements View.OnC
     }if (v == mCreateUserButton){
         createNewUser();
         }
+        if (v == mPasswordLoginButton){
+            loginWithPassword();
+        }
     }
 
     private void createNewUser(){
@@ -64,16 +76,58 @@ public class CreateAccountActivity extends AppCompatActivity implements View.OnC
         final String email = mEmailEditText.getText().toString().trim();
         String password = mPasswordEditText.getText().toString().trim();
         String confirmPassword = mConfirmPasswordEditText.getText().toString().trim();
+        mName = mNameEditText.getText().toString().trim();
+
+        boolean validEmail = isValidEmail(email);
+//        boolean validName = isValidName(name);
+        boolean validName = isValidName(mName);
+        boolean validPassword = isValidPassword(password, confirmPassword);
+        if (!validEmail || !validName || !validPassword) return;
+
+        mAuthProgressDialog.show();
 
         mFirebaseAuth.createUserWithEmailAndPassword(email, password)
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
+                        mAuthProgressDialog.dismiss();
+
                         if(task.isSuccessful()){
-                            Log.d(TAG,"Task is successful" );
+                            Log.d(TAG,"Authentication is successful" );
+                            createFirebaseUserProfile(task.getResult().getUser());
                         }else {
                             Toast.makeText(CreateAccountActivity.this, "Authentication failed", Toast.LENGTH_SHORT).show();
                         }
+                    }
+                });
+    }
+
+    private void createAuthProgressDialog(){
+        mAuthProgressDialog = new ProgressDialog(this);
+        mAuthProgressDialog.setTitle("Loading ...");
+        mAuthProgressDialog.setMessage("Authenticating with Firebase...");
+        mAuthProgressDialog.setCancelable(false);
+    }
+
+    private void loginWithPassword(){
+        String email = mEmailEditText.getText().toString().trim();
+        String password = mPasswordEditText.getText().toString().trim();
+
+        if (email == ""){
+            mEmailEditText.setError("PLease enter your email");
+        }
+        if (password == ""){
+            mPasswordEditText.setError("Password cannot be blank");
+            return;
+        }
+
+        mAuthProgressDialog.show();
+        mFirebaseAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task){
+                        mAuthProgressDialog.dismiss();
                     }
                 });
     }
@@ -82,8 +136,9 @@ public class CreateAccountActivity extends AppCompatActivity implements View.OnC
         mAuthStateListener = new FirebaseAuth.AuthStateListener() {
             @Override
             public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-                final FirebaseUser user = firebaseAuth.getCurrentUser();
+                FirebaseUser user = firebaseAuth.getCurrentUser();
                 if (user != null){
+                    getSupportActionBar().setTitle("Welcome, "+ user.getDisplayName() + "!");
                     Intent intent = new Intent(CreateAccountActivity.this, MainActivity.class);
                     intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
                     startActivity(intent);
@@ -92,6 +147,52 @@ public class CreateAccountActivity extends AppCompatActivity implements View.OnC
             }
         };
     }
+
+    private boolean isValidEmail(String email) {
+        boolean isGoodEmail =
+                (email != null && android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches());
+        if (!isGoodEmail) {
+            mEmailEditText.setError("Please enter a valid email address");
+            return false;
+        }
+        return isGoodEmail;
+    }
+
+    private boolean isValidName(String name) {
+        if (name.equals("")) {
+            mNameEditText.setError("Please enter your name");
+            return false;
+        }
+        return true;
+    }
+
+    private boolean isValidPassword(String password, String confirmPassword) {
+        if (password.length() < 6) {
+            mPasswordEditText.setError("Please create a password containing at least 6 characters");
+            return false;
+        } else if (!password.equals(confirmPassword)) {
+            mPasswordEditText.setError("Passwords do not match");
+            return false;
+        }
+        return true;
+    }
+
+    private void createFirebaseUserProfile(final FirebaseUser firebaseUser){
+        UserProfileChangeRequest addProfileName = new UserProfileChangeRequest.Builder()
+                .setDisplayName(mName).build();
+
+        firebaseUser.updateProfile(addProfileName).addOnCompleteListener(new OnCompleteListener<Void>() {
+
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if (task.isSuccessful()){
+                    Log.d(TAG, firebaseUser.getDisplayName()    );
+
+                }
+            }
+        });
+    }
+
 
     @Override
     public void onStart(){
